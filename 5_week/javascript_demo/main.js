81bf18e8aacd4a52da77236d342120a23b37a0b1
ronttizz <toni.nissinen@gmail.com>
2022-02-17 14:10:41 +0200

    Responsiveness practices with javascript

---
+++ 5_week/javascript_demo/main.js
@@ -0,0 +1,35 @@
+const header = document.querySelector("header");
+const button = document.querySelector("#backtotop");
+const mobButton = document.querySelector("#mobileButton");
+const nav = document.querySelector("nav");
+
+window.onscroll = () => {
+  scrollFunction();
+};
+
+scrollFunction = () => {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.classList.add("bg");
+    button.style.display = "block";
+  } else {
+    header.classList.remove("bg");
+    button.style.display = "none";
+  }
+};
+
+const getToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+
+button.addEventListener("click", getToTop);
+
+const mobMenu = () => {
+  if (nav.classList.contains("responsive")) {
+    nav.classList.remove("responsive");
+  } else {
+    nav.classList.add("responsive");
+  }
+};
+
+mobButton.addEventListener("click", mobMenu);
